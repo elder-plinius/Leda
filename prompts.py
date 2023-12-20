6bf9c01c9700f22da9c366b503347d328470caec
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-12-19 16:51:21 -0800

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -3,14 +3,14 @@
 
 # Prompt for Role Identification Agent
 ROLE_IDENTIFICATION_PROMPT = """
-Based on the user's input idea: '{user_idea}', identify and list the specific types of agents needed for the multi-agent AI system. Describe their roles, responsibilities, and capabilities, emphasizing the strengths of AI models.
-Output Format: A detailed list of agent types with descriptions of their roles and capabilities, formatted in bullet points or a numbered list.
+Based on the following idea: '{user_idea}', identify and list the specific types of agents needed to create a complete team of autonomous AI agents. Detail their roles, responsibilities, and capabilities, being sure to play to the strengths of AI models.
+Output Format: A list of a balanced, complete team of agent types with brief descriptions of their roles and capabilities, and arrange them in an order that makes logical sense for the workflow/task at had and formatted in bullet points or a numbered list.
 """
 
 # Prompt for Agent Configuration
 AGENT_CONFIGURATION_PROMPT = """
-Given the identified agent roles: '{agent_roles}', create internal instructions for each agent type. These instructions should be specific to the agent's functions, considering the context and objectives of the multi-agent system.
-Output Format: A Python script section with constant names for each set of internal instructions, formatted with triple quotes surrounding each instruction set.
+Given these identified agent roles: '{agent_roles}', write Custom Instructions/System Prompts for each agent type. Ensure that each prompt is tailored to the specific functionalities of the agent, considering the operational context and objectives of the team.
+Output Format: A single Python file of the whole agent team with capitalized constant names for each prompt, an equal sign between each agent name and their prompt, and triple quotes surrounding the prompt content. Follow best-practice prompting standards.
 """
 
 # Prompt for System Assembly
