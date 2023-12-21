a1e7c775377b728495edb058d465d6640646e83c
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-12-21 14:39:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 import google.generativeai as genai
-import prompts
+import prompts  # Ensure this imports your existing prompts file
 
 # Load environment variables
 load_dotenv()
@@ -14,25 +14,32 @@ genai.configure(api_key=google_api_key)
 # Initialize the Gemini-Pro model
 model = genai.GenerativeModel('gemini-pro')
 
-def gemini_pro_completion(prompt):
+def gemini_pro_completion(prompt, temperature=0.0123):
     try:
-        response = model.generate_content(prompt)
-        return response.text.strip()
+        print(f"Sending prompt to Gemini with temperature {temperature}: {prompt}")
+        response = model.generate_content(prompt, generation_config=genai.types.GenerationConfig(temperature=temperature))
+        result = response.text.strip()
+        print(f"Received from Gemini: {result}")
+        return result
     except Exception as e:
         print(f"Error in Gemini-Pro completion: {e}")
         return None
 
-def generate_system_script_content(user_idea):
-    role_identification_prompt = prompts.AGENT_ROLE_IDENTIFICATION_AGENT_PROMPT.format(user_idea=user_idea)
-    role_identification = gemini_pro_completion(role_identification_prompt)
+def generate_agent_roles(user_idea):
+    prompt = prompts.ROLE_IDENTIFICATION_PROMPT.format(user_idea=user_idea)
+    return gemini_pro_completion(prompt)
 
-    agent_configuration_prompt = prompts.AGENT_CONFIGURATION_AGENT_PROMPT.format(agent_roles=role_identification)
-    agent_configuration = gemini_pro_completion(agent_configuration_prompt)
+def generate_agent_prompts(agent_roles):
+    prompt = prompts.AGENT_CONFIGURATION_PROMPT.format(agent_roles=agent_roles)
+    return gemini_pro_completion(prompt)
 
-    system_assembly_prompt = prompts.SYSTEM_ASSEMBLY_AGENT_PROMPT.format(agent_sops=agent_configuration)
-    system_assembly = gemini_pro_completion(system_assembly_prompt)
+def generate_script_plan(agent_prompts, user_idea):
+    prompt = prompts.SCRIPT_PLANNING_PROMPT.format(agent_prompts=agent_prompts, user_idea=user_idea)
+    return gemini_pro_completion(prompt)
 
-    return f"# System Script for {user_idea}\n\n{system_assembly}"
+def generate_system_script(script_plan, agent_prompts):
+    prompt = prompts.SYSTEM_ASSEMBLY_PROMPT.format(script_plan=script_plan, agent_prompts=agent_prompts)
+    return gemini_pro_completion(prompt)
 
 def save_to_file(filename, content):
     with open(filename, 'w') as file:
@@ -46,10 +53,13 @@ def main():
     user_idea = input("Enter your idea for the multi-agent system: ")
     timestamp = get_timestamp()
 
-    system_script_content = generate_system_script_content(user_idea)
+    agent_roles = generate_agent_roles(user_idea)
+    agent_prompts = generate_agent_prompts(agent_roles)
+    script_plan = generate_script_plan(agent_prompts, user_idea)
+    system_script = generate_system_script(script_plan, agent_prompts)
 
-    # Save the generated system script to a timestamped file
-    save_to_file(f'system_script_{timestamp}.py', system_script_content)
+    save_to_file(f'agent_prompts_{timestamp}.py', agent_prompts)
+    save_to_file(f'system_script_{timestamp}.py', system_script)
 
 if __name__ == "__main__":
     main()
